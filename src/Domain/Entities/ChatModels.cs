daec134e7c4b90f17fc58bc44b31bdbb9b3f0313
SardorSohinazarov <sardorstudent0618@gmail.com>
2026-07-12 04:49:23 +0500

    infra added

--- src/Domain/Entities/ChatModels.cs
+++ src/Domain/Entities/ChatModels.cs
@@ -1,48 +1,6 @@
+using Domain.Entities;
+
 namespace API.Models;
-
-public enum ChatType
-{
-    Private,
-    Group,
-    Channel,
-    Secret
-}
-
-public enum ChatMemberRole
-{
-    Member,
-    Admin,
-    Creator
-}
-
-public enum PollType
-{
-    Regular,
-    Quiz,
-    MultipleChoice
-}
-
-public enum CallType
-{
-    Audio,
-    Video
-}
-
-public enum AttachmentType
-{
-    Photo,
-    Video,
-    File,
-    Sticker
-}
-
-public enum SubscriptionStatus
-{
-    Active,
-    Canceled,
-    Expired,
-    Pending
-}
 
 public class User
 {
---
+++ src/Domain/Entities/Enums.cs
@@ -0,0 +1,45 @@
+﻿namespace Domain.Entities;
+
+public enum ChatType
+{
+    Private,
+    Group,
+    Channel,
+    Secret
+}
+
+public enum ChatMemberRole
+{
+    Member,
+    Admin,
+    Creator
+}
+
+public enum PollType
+{
+    Regular,
+    Quiz,
+    MultipleChoice
+}
+
+public enum CallType
+{
+    Audio,
+    Video
+}
+
+public enum AttachmentType
+{
+    Photo,
+    Video,
+    File,
+    Sticker
+}
+
+public enum SubscriptionStatus
+{
+    Active,
+    Canceled,
+    Expired,
+    Pending
+}
